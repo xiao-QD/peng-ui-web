316c99c11a2f95b6fdd8a7e98017ab3c87733309
xiao-QD <1422581843@qq.com>
2022-10-12 21:43:54 +0800

    项目中途迁移，即将开始tabs组件库

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>示例</h1>
+  <div>
+    <Switch v-model:value="bool" />
+  </div>
+</template>
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+import { ref } from "vue";
+export default {
+  components: { Switch },
+  setup() {
+    const bool = ref(false);
+    return { bool };
+  },
+};
+</script>
