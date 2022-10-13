72cc3d5ead5220e58075a74aa5514e5dbf4d23ac
xiao-QD <1422581843@qq.com>
2022-10-13 10:32:46 +0800

    初步创建tabs组件与tab组件

---
+++ src/lib/Tab.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>tab</h1>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,7 @@
+<template>woshi tabs</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
