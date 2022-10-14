f1cf8f6e30cd6d7e234d1f689bf7018c5976d317
xiao-QD <1422581843@qq.com>
2022-10-14 16:23:40 +0800

    实现导航切换

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>Tabs 示例111</div>
-  <Tabs>
-    <Tab title="导航2">内容1</Tab>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
 </template>
@@ -9,10 +9,16 @@
 <script lang="ts">
 import Tabs from "../lib/Tabs.vue";
 import Tab from "../lib/Tab.vue";
+import { ref } from "vue";
 export default {
   components: {
     Tabs,
     Tab,
+  },
+
+  setup() {
+    const x = ref("导航1");
+    return { x };
   },
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>
-    <div v-for="(item, index) in titles" :key="index">{{ item }}</div>
-    <component v-for="item in defaults" :is="item"></component>
+  <div class="peng-tabs">
+    <!-- 导航区 -->
+    <div class="peng-tabs-nav">
+      <div
+        class="peng-tabs-nav-item"
+        :class="{ selected: item == selected }"
+        v-for="(item, index) in titles"
+        :key="index"
+        @click="select(item)"
+      >
+        {{ item }}
+      </div>
+    </div>
+    <!-- 内容区 -->
+    <div class="peng-tabs-content">
+      <component
+        class="peng-tabs-content-item"
+        :is="current"
+        :key="current.props.title"
+      ></component>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from "./Tab.vue";
+import { computed } from "vue";
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
     //defaults获取插槽内的东西
     const defaults = context.slots.default();
@@ -23,9 +47,47 @@ export default {
       return item.props.title;
     });
 
-    return { defaults, titles };
+    //记录一下当前被选中的tab，谁被选中就展示谁的内容
+    //filter返回的是一个数组，我们要的是一个变量，所以在最后加一个[0]进行取值
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title == props.selected;
+      })[0];
+    });
+
+    //定义选择函数，用户点击某个标签，就通知父组件渲染相应tag的内容
+    const select = (title: String) => {
+      context.emit("update:selected", title);
+    };
+
+    return { defaults, titles, current, select };
   },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.peng-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
