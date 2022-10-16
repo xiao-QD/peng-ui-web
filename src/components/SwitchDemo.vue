978401c6eb68dada743e9f0d1e2d87e1bfe82eed
xiao-QD <1422581843@qq.com>
2022-10-16 15:35:11 +0800

    实现代码的展示

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,28 +1,37 @@
 <template>
   <div>
     <h1>Switch 组件示例</h1>
+    <!-- 上半部分 -->
     <div class="demo">
       <h2>常规用法</h2>
+
       <div class="demo-component">
-        <Switch1Demo></Switch1Demo>
+        <Switch1Demo />
       </div>
+
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
+
       <div class="demo-code">
-        <!-- <pre>{{ Switch1Demo.__sourceCode }}</pre> -->
+        <pre>{{ Switch1Demo.__sourceCode }}</pre>
       </div>
     </div>
+
+    <!-- 下半部分 -->
     <div class="demo">
       <h2>支持 disabled</h2>
+
       <div class="demo-component">
-        <Switch2Demo></Switch2Demo>
+        <Switch2Demo />
       </div>
+
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
+
       <div class="demo-code">
-        <!-- <pre>{{ Switch2Demo.__sourceCode }}</pre> -->
+        <pre>{{ Switch2Demo.__sourceCode }}</pre>
       </div>
     </div>
   </div>
@@ -30,13 +39,15 @@
 <script lang="ts">
 import Switch from "../lib/Switch.vue";
 import Button from "../lib/Button.vue";
+import Switch1Demo from "./Switch1.demo.vue";
+import Switch2Demo from "./Switch2.demo.vue";
 
 import { ref } from "vue";
 export default {
-  components: { Switch, Button },
+  components: { Switch, Button, Switch1Demo, Switch2Demo },
   setup() {
     const bool = ref(false);
-    return { bool };
+    return { bool, Switch1Demo, Switch2Demo };
   },
 };
 </script>
